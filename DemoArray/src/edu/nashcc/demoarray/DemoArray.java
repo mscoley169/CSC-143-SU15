4a1d04baf6a1e892f77a848af1f662d8fea8025a
mscoley169 <mscoley169@st.nashcc.edu>
2015-05-28 11:16:50 -0400

    intro ch8 assignment DemoArray

---
+++ DemoArray/src/edu/nashcc/demoarray/DemoArray.java
@@ -0,0 +1,29 @@
+/*	Name: Matthew S. Coley
+ *	Date: 28 May 2015
+ *	Assignment: You Do It/Chapter 8 demo array
+ */
+
+package edu.nashcc.demoarray;
+
+public class DemoArray {
+
+	public static void main(String[] args) {
+
+		double[] salaries = new double[4];
+		salaries[0] = 6.25;
+		salaries[1] = 6.55;
+		salaries[2] = 10.25;
+		salaries[3] = 16.85;
+		
+		System.out.println("Salaries one by one are: ");
+		System.out.println(salaries[0]);
+		System.out.println(salaries[1]);
+		System.out.println(salaries[2]);
+		System.out.println(salaries[3]);
+		
+		// intentional error
+		// System.out.println(salaries[4]);
+		
+	}
+
+}
