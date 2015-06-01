384ccec306022365bd23a8f66f3108e4b1273713
mscoley169 <mscoley169@st.nashcc.edu>
2015-06-01 11:16:48 -0400

    Ch8 Debug added
    1-3 complete, 4 unfinished

---
+++ DebugEight4/src/edu/nashcc/dbg8/DebugEight4.java
@@ -0,0 +1,35 @@
+package edu.nashcc.dbg8;
+
+// Allows user to enter a series of words
+// and displays them in reverse order
+import javax.swing.*;
+public class DebugEight4
+{
+   public static void main(String[] args)
+   {
+      int x = 0, y;
+      String array[] = new String[100];
+      String entry;
+      final String STOP = "XXX";
+      StringBuffer message = new
+          StringBuffer("The words in reverse order are\n");
+     
+      entry = JOptionPane.showInputDialog(null,
+        "Enter any word\n" +
+        "Enter " + STOP + " when you want to stop"); 
+      while(!(entry.equals(STOP)))
+      {
+         array[x] = entry;
+         entry = JOptionPane.showInputDialog(null,
+            "Enter another word\n" +
+            "Enter " + STOP + " when you want to stop"); 
+      }
+      for(y = 0; y < 0; ++y)
+      {
+         message.append(array[y]);
+         message.append("\n");
+      }
+      JOptionPane.showMessageDialog(null, message);
+   }
+}
+
