26baba7f3879549a6e372a44e2c96ea55da26e9e
mscoley169 <mscoley169@st.nashcc.edu>
2015-06-13 15:37:38 -0400

    DebugTen2.java added/unfinished

---
+++ DebugTen2/src/edu/nashcc/debug/DebugTen2.java
@@ -0,0 +1,17 @@
+package edu.nashcc.debug;
+
+// A Vacation is 10 days
+// but an ExtendedVacation is 30 days
+public class DebugTen2
+{
+   public static void main(String args[]) 
+   {
+      DebugVacation myVacation = new DebugVacation();
+      DebugExtendedVacation yourVacation =
+         new DebugExtendedVacation();
+      System.out.println("My vacation is for " +
+         myVacation.getDays() + " days");
+      System.out.println("Your vacation is for " +
+         yourVacation.getDays() + " days");
+   }
+}
