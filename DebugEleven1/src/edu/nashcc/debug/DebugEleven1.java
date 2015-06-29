833fa4df59c311ffc12b46595424d3c29a6ab060
mscoley169 <mscoley169@st.nashcc.edu>
2015-06-29 10:53:38 -0400

    DebugEleven etc

---
+++ DebugEleven1/src/edu/nashcc/debug/DebugEleven1.java
@@ -0,0 +1,12 @@
+package edu.nashcc.debug;
+
+// Instantiates Rowboat
+// Rowboat is child of Boat
+public class DebugEleven1
+{
+   public static void main(String[] args)
+   {
+      DebugRowboat myBoat = new DebugRowboat();
+      System.out.println(myBoat.toString());
+   }
+}
