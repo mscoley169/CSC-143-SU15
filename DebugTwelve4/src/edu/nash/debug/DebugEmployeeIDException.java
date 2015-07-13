0960b1fcc4beff2e7273003ed3d2c5ca3bc15fd7
mscoley169 <mscoley169@st.nashcc.edu>
2015-07-13 09:56:13 -0400

    DebugTwelve1-4 completed

---
+++ DebugTwelve4/src/edu/nash/debug/DebugEmployeeIDException.java
@@ -0,0 +1,16 @@
+/*	Matthew S. Coley
+ * 	Ch 12 debugs
+ * 	13 July 2015
+ */
+
+package edu.nash.debug;
+
+public class DebugEmployeeIDException extends Exception
+{
+   public DebugEmployeeIDException()
+   {
+      super();
+   }
+}
+
+
