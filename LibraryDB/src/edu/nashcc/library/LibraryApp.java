4aefebfdfd3843e9e1197f1d13460e46107ddac5
mscoley169 <mscoley169@st.nashcc.edu>
2015-07-21 21:13:39 -0400

    e

--- LibraryDB/src/edu/nashcc/library/LibraryApp.java
+++ LibraryDB/src/edu/nashcc/library/LibraryApp.java
@@ -3,10 +3,22 @@ package edu.nashcc.library;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 
 public class LibraryApp {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +50,7 @@ public class LibraryApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
 
+	}
 }
